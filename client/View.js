186eb7ed7b8de4f3a3d7d37c0f72b1ff4559aa45
Karl Kroening <karlk@kralnet.us>
2013-06-06 23:08:11 -0500

    Fix View position and size

--- client/View.js
+++ client/View.js
@@ -2,6 +2,9 @@ function View(width, height) {
     this.width  = width;
     this.height = height;
     this.div    = $('<div></div>');
+    this.div.css("position", "absolute");
+    this.div.css("width", "100%");
+    this.div.css("height", "100%");
 }
 
 View.prototype.setWidth = function(width) {
